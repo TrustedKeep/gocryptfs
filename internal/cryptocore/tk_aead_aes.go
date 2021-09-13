01f92a86a2125711b812cb2699b83ba316c4f88c
Keith Schaffer <keith.schaffer@trustedconcepts.com>
2021-09-13 10:35:51 -0400

    limit amount encrypted per key

--- internal/cryptocore/tk_aead_aes.go
+++ internal/cryptocore/tk_aead_aes.go
@@ -3,7 +3,9 @@ package cryptocore
 import (
 	"crypto/aes"
 	"crypto/cipher"
+	"encoding/binary"
 	"encoding/hex"
+	"fmt"
 	"sync"
 	"time"
 
@@ -16,6 +18,8 @@ import (
 const (
 	keyCacheSize  = 1000
 	keyExpiration = time.Minute * 5
+	bytesPerKey   = 1024 * 1024 * 1024 * 30 // 30 Gb per encryption key
+	blockSize     = 4096                    // should match contentenc.DefaultBS
 )
 
 var _ cipher.AEAD = &gcmAead{}
@@ -89,15 +93,8 @@ func (t *gcmAead) getKey(ad []byte) []byte {
 	t.keyMu.Lock()
 	defer t.keyMu.Unlock()
 
-	// from content.go/concatAD, the AD passed in contains the fileID and block# as:
-	// ad = [blockNo.bigEndian fileID]
-	// so, first 8 bytes are bigendian uint64 containing block ID
-	// next 8 bytes are the file identifier
-	// TODO:
-	// should generate a new key based on block to make sure we don't go over 60GB with a single key
-	// something like 60GB / contentenc.DefaultBS
-	id := hex.EncodeToString(ad[8:])
-	// blockNum := binary.BigEndian.Uint64(ad[:8])
+	id := getKeyName(ad)
+	tlog.Debug.Printf("Retrieving key %s", id)
 
 	if iKey, cached := t.keys.Get(id); cached {
 		return iKey.([]byte)
@@ -112,4 +109,20 @@ func (t *gcmAead) getKey(ad []byte) []byte {
 		t.keys.Add(id, key)
 		return key
 	}
+}
+
+// getKeyName returns the name of the key in the KMS for a given file/block.  This information
+// is encoded in the additionalData parameter passed to the seal function.  Key name is in the form
+// fileID/block, where fileID is the file node and block is calculated based on the number of
+// bytes to encrypt with a single key...so, ~30Gb with 1 key means the first ~7 million blocks use the
+// same encryption key
+func getKeyName(additionalData []byte) string {
+	// from content.go/concatAD, the AD passed in contains the fileID and block# as:
+	// ad = [blockNo.bigEndian fileID]
+	// so, first 8 bytes are bigendian uint64 containing block ID
+	// next 8 bytes are the file identifier
+	id := hex.EncodeToString(additionalData[8:])
+	blockNum := binary.BigEndian.Uint64(additionalData[:8])
+	blockKey := (int(blockNum) * blockSize) / bytesPerKey
+	return fmt.Sprintf("%s/%d", id, blockKey)
 }
