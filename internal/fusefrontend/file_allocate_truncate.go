bfb81411c312502b5e696bbdcaeb8e436c419e06
Steven Halaka <steven.halaka@twosixtech.com>
2024-09-10 08:47:34 -0400

    fix: ensure ftruncate writes envelope

--- internal/fusefrontend/file_allocate_truncate.go
+++ internal/fusefrontend/file_allocate_truncate.go
@@ -202,6 +202,13 @@ func (f *File) truncateGrowFile(oldPlainSz uint64, newPlainSz uint64) syscall.Er
 				return fs.ToErrno(err)
 			}
 			f.fileTableEntry.ID = id
+			//set up the envelope key if needed
+			if f.rootNode.args.Envelope {
+				err = f.initializeEnvelopeKey()
+				if err != nil {
+					return syscall.EIO
+				}
+			}
 		}
 		cSz := int64(f.contentEnc.PlainSizeToCipherSize(newPlainSz))
 		err := syscall.Ftruncate(f.intFd(), cSz)
